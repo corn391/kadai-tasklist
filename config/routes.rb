110bc6848aee6823dcc6cf2cd979d59672620f96
corn <corn.mw7kc48kcx@outlook.jp>
2020-10-07 14:23:19 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'session/new'
+  get 'session/create'
+  get 'session/destroy'
     root to: 'tasks#index'
+    
+     get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
     
     get 'signup', to: 'users#new'
     resources :users, only: [:index, :show, :new, :create]
