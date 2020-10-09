99b2b5e12d6cafa0630260e7e2cfd5f801c6386f
corn <corn.mw7kc48kcx@outlook.jp>
2020-10-09 06:40:31 +0000

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get 'session/new'
-  get 'session/create'
-  get 'session/destroy'
     root to: 'tasks#index'
-    
+   
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
     
     get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :new, :create]
+    resources :users
     resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
